4561aea9744b615e40a10e962f11e496d27fe552
LouVang97 <35350104+LouVang97@users.noreply.github.com>
2018-01-31 21:12:39 -0600

    Update Ch3.sh

--- Ch3.sh
+++ Ch3.sh
@@ -4,5 +4,5 @@ touch file87 file88
 echo blah blah > /dev/null
 ls -l
 dd if=file87 of=fiile88 ibs=1k obs=1k count=2
-sudo /llib/udev/ata_id --export /dev/sda
+sudo /lib/udev/ata_id --export /dev/sda
 
